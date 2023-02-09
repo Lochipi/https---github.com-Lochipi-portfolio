3d4ccad173475ad14e9850245a79e31adfd4276a
Lochipi <corneliuslochipi@gmail.com>
2023-02-09 17:36:58 +0300

    modified all the files

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// helllow world,, we're just getting started:>
+// Time display footer
+const weekday = ["Sunday!", "Monday!", "Tuesday!", "Wednesday!", "Thursday!", "Friday!", "Saturday!"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("displaydate").innerHTML = day;
