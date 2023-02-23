3f00ed317c814dc4b4f43fbb79918f06d5d13519
Cornelius Emase <corneliuslochipi@gmail.com>
2023-02-23 15:50:53 +0300

    created contact page

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 // Time display footer
-const weekday = ["Sunday!", "Monday!", "Tuesday!", "Wednesday!", "Thursday!", "Friday!", "Saturday!"];
+// const weekday = ["Sunday!", "Monday!", "Tuesday!", "Wednesday!", "Thursday!", "Friday!", "Saturday!"];
 
-const d = new Date();
-let day = weekday[d.getDay()];
-document.getElementById("displaydate").innerHTML = day;
+// const d = new Date();
+// let day = weekday[d.getDay()];
+// document.getElementById("displaydate").innerHTML = day;
+
+
+
+// js tests
+// const name = "Emase"
+// const age = 20
+// let sentense = `Hello my name is ${name} and I'm ${age} years old`
+// console.log(sentense)
+
+// function fullname(first, second){
+//     return `my name is ${first} ${second}`
+// }
+// console.log(`${fullname("john", "doe")}`)
+
+// numbers = [1,29,3,4,5,7]
+
+// biggest = `the biggest number is ${Math.max(...numbers)}`
+// console.log(biggest);
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  const element = <h1>Hello, world</h1>;
+  root.render(element);
