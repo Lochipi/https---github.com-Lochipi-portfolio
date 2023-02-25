9d6c8d77e9652a0e50be68a7ed3632bfd6236e3e
Cornelius Emase <corneliuslochipi@gmail.com>
2023-02-25 14:09:09 +0300

    made changes on skills section

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 // Time display footer
-// const weekday = ["Sunday!", "Monday!", "Tuesday!", "Wednesday!", "Thursday!", "Friday!", "Saturday!"];
+const weekday = ["Sunday!", "Monday!", "Tuesday!", "Wednesday!", "Thursday!", "Friday!", "Saturday!"];
 
-// const d = new Date();
-// let day = weekday[d.getDay()];
-// document.getElementById("displaydate").innerHTML = day;
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("displaydate").innerHTML = day;
 
 
+// toggle function on js 
+function showSection(sectionId) {
+  var section1 = document.getElementById("section1");
+  var section2 = document.getElementById("section2");
+  var button1 = document.getElementById("button1");
+  var button2 = document.getElementById("button2");
 
-// js tests
-// const name = "Emase"
-// const age = 20
-// let sentense = `Hello my name is ${name} and I'm ${age} years old`
-// console.log(sentense)
-
-// function fullname(first, second){
-//     return `my name is ${first} ${second}`
-// }
-// console.log(`${fullname("john", "doe")}`)
-
-// numbers = [1,29,3,4,5,7]
-
-// biggest = `the biggest number is ${Math.max(...numbers)}`
-// console.log(biggest);
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-  );
-  const element = <h1>Hello, world</h1>;
-  root.render(element);
+  if (sectionId === "section1") {
+    section1.style.display = "block";
+    section2.style.display = "none";
+    button1.disabled = true;
+    button2.disabled = false;
+  } else if (sectionId === "section2") {
+    section1.style.display = "none";
+    section2.style.display = "block";
+    button1.disabled = false;
+    button2.disabled = true;
+  }
+}
